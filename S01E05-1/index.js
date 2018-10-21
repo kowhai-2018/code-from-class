b01f48c4e6c5992131ee85b860094a424229b8ef
Bryce Kehoe <bryce@kehoe.co.nz>
2018-10-22 11:41:18 +1300

    S01E05-1 Shifted code around for consistency

--- S01E05-1/index.js
+++ S01E05-1/index.js
@@ -1,32 +1,7 @@
 const server = require('./server')
-const path = require('path')
 
 const PORT = 3000
 
 server.listen(PORT, function () {
   console.log('Listening on port', PORT, '...')
-})
-
-server.get('/', (req, res) => {
-  console.log('Welcome to the internet')
-  res.send('Welcome to the internet')
-})
-
-server.get('/kowhai', (req, res) => {
-  console.log('Kowhai is cool!')
-  res.send('<h2>Kowhai rocks</h2>')
-})
-
-server.get('/hi/:name', (req, res) => {
-  console.log('Hitting /hi')
-
-  const name = req.params.name
-
-  res.send('Hi ' + name + '!')
-})
-
-server.get('/faq', (req, res) => {
-  console.log('Hitting /faq')
-
-  res.sendFile(path.join(__dirname, './faq.html'))
 })
--- S01E05-1/server.js
+++ S01E05-1/server.js
@@ -1,7 +1,31 @@
+const path = require('path')
 const express = require('express')
-
 const server = express()
+
+module.exports = server
 
 server.use(express.static('public'))
 
-module.exports = server
+server.get('/', (req, res) => {
+  console.log('Welcome to the internet')
+  res.send('Welcome to the internet')
+})
+
+server.get('/kowhai', (req, res) => {
+  console.log('Kowhai is cool!')
+  res.send('<h2>Kowhai rocks</h2>')
+})
+
+server.get('/hi/:name', (req, res) => {
+  console.log('Hitting /hi')
+
+  const name = req.params.name
+
+  res.send('Hi ' + name + '!')
+})
+
+server.get('/faq', (req, res) => {
+  console.log('Hitting /faq')
+
+  res.sendFile(path.join(__dirname, './faq.html'))
+})
